72dd86c0a0fa020c73ca4bf4b73653997a280f20
Bert Melis <bertmelis@users.noreply.github.com>
2020-05-05 19:49:15 +0200

    Fix travis
    + add testing for FC16

--- tests/Test_ModbusMessage.cpp
+++ tests/Test_ModbusMessage.cpp
@@ -4,6 +4,7 @@
 
 #include "Includes/catch.hpp"
 #include "Includes/CheckArray.h"
+#include <cstring>
 
 using Catch::Matchers::WithinAbs;
 
@@ -12,6 +13,37 @@ TEST_CASE("Read input registers", "[FC04]") {
   uint8_t stdMessage[] = {0x11, 0x04, 0x00, 0x08, 0x00, 0x01, 0xB2, 0x98};
   uint8_t stdResponse[] = {0x11, 0x04, 0x02, 0x00, 0x0A, 0xF8, 0xF4};
   uint8_t stdErrorResponse[] = {0x11, 0x84, 0x02, 0x00, 0x00};  // TODO(bertmelis): adjust CRC
+
+  REQUIRE(request->getSize() == sizeof(stdMessage));
+  REQUIRE_THAT(request->getMessage(), ByteArrayEqual(stdMessage, sizeof(stdMessage)));
+
+  esp32ModbusRTUInternals::ModbusResponse* response = new esp32ModbusRTUInternals::ModbusResponse(request->responseLength(), request);
+
+  SECTION("normal response") {
+    for (uint8_t i = 0; i < sizeof(stdResponse); ++i) {
+      response->add(stdResponse[i]);
+    }
+    CHECK(response->isSucces());
+  }
+
+  SECTION("error response") {
+    for (uint8_t i = 0; i < sizeof(stdErrorResponse); ++i) {
+      response->add(stdErrorResponse[i]);
+    }
+    CHECK_FALSE(response->isSucces());
+    CHECK(response->getError() == esp32Modbus::ILLEGAL_DATA_ADDRESS);
+  }
+
+  delete request;
+  delete response;
+}
+
+TEST_CASE("Write multiple holding registers", "[FC16]") {
+  uint8_t data[] = {0x00, 0x0A, 0x01, 0x02};
+  esp32ModbusRTUInternals::ModbusRequest* request = new esp32ModbusRTUInternals::ModbusRequest16(0x11, 0x0001, 0x0002, data);
+  uint8_t stdMessage[] = {0x11, 0x10, 0x00, 0x01, 0x00, 0x02, 0x04, 0x00, 0x0A, 0x01, 0x02, 0xC6, 0xF0};
+  uint8_t stdResponse[] = {0x11, 0x10, 0x00, 0x01, 0x00, 0x02, 0x12, 0x98};
+  uint8_t stdErrorResponse[] = {0x11, 0x90, 0x02, 0x00, 0x00};  // TODO(bertmelis): adjust CRC
 
   REQUIRE(request->getSize() == sizeof(stdMessage));
   REQUIRE_THAT(request->getMessage(), ByteArrayEqual(stdMessage, sizeof(stdMessage)));
